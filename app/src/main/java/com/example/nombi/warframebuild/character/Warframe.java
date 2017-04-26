8c4cdf1b4ba09ea31134a2ee7e540a3fbc478f2c
Calvin Gunther <Calvin Gunther>
2017-04-26 11:33:59 -0700

    Added Character/Weapon Java classes

---
+++ app/src/main/java/com/example/nombi/warframebuild/character/Warframe.java
@@ -0,0 +1,8 @@
+package com.example.nombi.warframebuild.character;
+
+/**
+ * Created by Calvin on 4/26/2017.
+ */
+
+public class Warframe {
+}
---
+++ app/src/main/java/com/example/nombi/warframebuild/weapon/CompanionWeapon.java
@@ -0,0 +1,8 @@
+package com.example.nombi.warframebuild.weapon;
+
+/**
+ * Created by Calvin on 4/26/2017.
+ */
+
+public class CompanionWeapon {
+}
