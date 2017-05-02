558554ca60446107da9f19fd08d0878a318eab74
cggaming <cdg57@uw.edu>
2017-05-02 13:51:08 -0700

    Testing, addition of two classes

---
+++ app/src/main/java/com/example/nombi/warframebuild/character/CharacterLoadout.java
@@ -0,0 +1,8 @@
+package com.example.nombi.warframebuild.character;
+
+/**
+ * Created by Calvin on 5/2/2017.
+ */
+
+public class CharacterLoadout {
+}
---
+++ app/src/main/java/com/example/nombi/warframebuild/weapon/WeaponLoadout.java
@@ -0,0 +1,8 @@
+package com.example.nombi.warframebuild.weapon;
+
+/**
+ * Created by Calvin on 5/2/2017.
+ */
+
+public class WeaponLoadout {
+}
