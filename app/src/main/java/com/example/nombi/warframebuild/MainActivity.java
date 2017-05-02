b76f962a944f5a65c807ee3a4c19be8e6ab91f16
Nombytes <castillo.athena0131@gmail.com>
2017-05-02 15:34:41 -0700

    adding in login screen

--- app/src/main/java/com/example/nombi/warframebuild/MainActivity.java
+++ app/src/main/java/com/example/nombi/warframebuild/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.nombi.warframebuild;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launch(View v){
+        DialogFragment fragment = null;
+
+
+
     }
 }
