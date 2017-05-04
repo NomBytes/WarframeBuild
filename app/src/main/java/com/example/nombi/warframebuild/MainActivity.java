09fd71ec05f4ffd662b96702531e908cbe76b9c2
Nombytes <castillo.athena0131@gmail.com>
2017-05-04 15:38:14 -0700

    working on login need to connect to database

--- app/src/main/java/com/example/nombi/warframebuild/MainActivity.java
+++ app/src/main/java/com/example/nombi/warframebuild/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.nombi.warframebuild;
 
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     Button login;
@@ -27,8 +31,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view){
+        this.email = (EditText)findViewById(R.id.email);
+        this.password = (EditText)findViewById(R.id.password);
+        DialogFragment fragment = null;
+
+        if(!validate(email.toString())){
+            fragment = new UserDialogFragment();
+            fragment.show(getSupportFragmentManager(), "launch");
+        }
+
+    }
+    public boolean validate(String s){
+
+        Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+        Matcher m = p.matcher(s);
+        boolean matchFound = m.matches();
+
+        return matchFound;
+    }
+
+    public void register(View v){
         email = (EditText)findViewById(R.id.email);
         password = (EditText)findViewById(R.id.password);
+
 
 
     }
