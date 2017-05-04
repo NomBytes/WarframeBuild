eecad5c1acf6825b6a94318e7b8ba8eb71b614d4
Nombytes <castillo.athena0131@gmail.com>
2017-05-04 14:24:10 -0700

    working on login need to connect to database

--- app/src/main/java/com/example/nombi/warframebuild/MainActivity.java
+++ app/src/main/java/com/example/nombi/warframebuild/MainActivity.java
@@ -5,19 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button login;
     Button register;
+    EditText email;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                login(v);
+
+            }
+        });
     }
 
+    public void login(View view){
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+
+
+    }
     public void launch(View v){
         DialogFragment fragment = null;
-
 
 
     }
