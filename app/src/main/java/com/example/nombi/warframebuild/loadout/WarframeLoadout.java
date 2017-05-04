fede4580e60ae69e327fc1433835aa31c7b0318f
cggaming <cdg57@uw.edu>
2017-05-04 13:14:41 -0700

    Added loadout class templates and added some colors for use in displaying mods.

---
+++ app/src/main/java/com/example/nombi/warframebuild/loadout/WarframeLoadout.java
@@ -0,0 +1,10 @@
+package com.example.nombi.warframebuild.loadout;
+
+/**
+ * A loadout for a Warframe.
+ * @author Calvin
+ * @version 17.05.04
+ */
+
+public class WarframeLoadout extends Loadout {
+}
