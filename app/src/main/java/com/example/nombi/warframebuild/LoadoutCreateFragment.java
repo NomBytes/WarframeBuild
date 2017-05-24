f086e57d83eb059c959d6c910963ee4850899375
Nombytes <castillo.athena0131@gmail.com>
2017-05-24 12:13:12 -0700

    warframe button should lead to list of warframes

--- app/src/main/java/com/example/nombi/warframebuild/LoadoutCreateFragment.java
+++ app/src/main/java/com/example/nombi/warframebuild/LoadoutCreateFragment.java
@@ -3,10 +3,12 @@ package com.example.nombi.warframebuild;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -22,6 +24,8 @@ public class LoadoutCreateFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    Fragment FRAG = new WarframeFragment();
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -64,8 +68,27 @@ public class LoadoutCreateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_loadout_create, container, false);;
 
-        return inflater.inflate(R.layout.fragment_loadout_create, container, false);
+        Button warframe_b = (Button) view.findViewById(R.id.warframe_button);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton)
+                getActivity().findViewById(R.id.fab);
+        floatingActionButton.hide();
+
+        warframe_b.setOnClickListener( new View.OnClickListener(){
+                public void onClick(View v){
+                    getActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.fragment_container, FRAG)
+                            .commit();
+
+                }
+            }
+        );
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
